ac20edd8f7d3ac60876b5fac5a598b147ad8cd5c
adriensalon <adrien.salon@live.fr>
2023-02-05 17:23:19 +0100

    renderer + propre go animation mtn

--- staticgui/include/staticgui/glue/renderer.hpp
+++ staticgui/include/staticgui/glue/renderer.hpp
@@ -42,11 +42,9 @@ namespace glue {
         void process_input(const std::any& input);
 
     private:
+        struct diligent_renderer;
+        std::unique_ptr<diligent_renderer> _renderer_impl = nullptr;
         std::array<float, 4> _clear_color = { 0.f, 0.f, 0.f, 1.f };
-        // std::any _render_device_impl = nullptr;
-        // std::any _swap_chain_impl = nullptr;
-        // std::any _device_context_impl = nullptr;
-        // std::any _imgui_renderer_impl = nullptr;
     };
 
 }
--- staticgui/source/glue/renderer.cpp
+++ staticgui/source/glue/renderer.cpp
@@ -49,44 +49,37 @@
 namespace staticgui {
 namespace glue {
 
-    static std::unique_ptr<Diligent::Win32NativeWindow> Window;
-    static Diligent::IRenderDevice* _render_device_impl;
-    static Diligent::IDeviceContext* _device_context_impl;
-    static Diligent::ISwapChain* _swap_chain_impl;
-    static Diligent::ImGuiImplSDL* _imgui_renderer_impl;
-    static SDL_Window* _sdl_window;
-
-    Diligent::IRenderDevice* get_render_device(std::any& untyped)
-    {
-        return std::any_cast<Diligent::IRenderDevice*>(untyped);
-    }
-
-    namespace detail {
-        // conversions comme window
-    }
+    struct renderer::diligent_renderer {
+        Diligent::RefCntAutoPtr<Diligent::IRenderDevice> render_device;
+        Diligent::RefCntAutoPtr<Diligent::IDeviceContext> device_context;
+        Diligent::RefCntAutoPtr<Diligent::ISwapChain> swap_chain;
+        std::unique_ptr<Diligent::ImGuiImplSDL> imgui_renderer = nullptr;
+        SDL_Window* sdl_window = nullptr;
+    };
 
     renderer::renderer(const window& existing_window)
     {
-        _sdl_window = existing_window.get_sdl_window();
+        _renderer_impl = std::make_unique<renderer::diligent_renderer>();
+        _renderer_impl->sdl_window = existing_window.get_sdl_window();
         Diligent::SwapChainDesc _swap_chain_descriptor;
         if constexpr (is_renderer_backend_directx) {
             Diligent::IEngineFactoryD3D11* pFactoryD3D11 = Diligent::GetEngineFactoryD3D11();
             Diligent::EngineD3D11CreateInfo _engine_create_info;
             // _engine_create_info.SetValidationLevel(Diligent::VALIDATION_LEVEL_2);
-            pFactoryD3D11->CreateDeviceAndContextsD3D11(_engine_create_info, &_render_device_impl, &_device_context_impl);
+            pFactoryD3D11->CreateDeviceAndContextsD3D11(_engine_create_info, &(_renderer_impl->render_device), &(_renderer_impl->device_context));
             Diligent::Win32NativeWindow _win32_native_window(existing_window.get_native_window());
-            pFactoryD3D11->CreateSwapChainD3D11(_render_device_impl, _device_context_impl, _swap_chain_descriptor, Diligent::FullScreenModeDesc {}, _win32_native_window, &_swap_chain_impl);
+            pFactoryD3D11->CreateSwapChainD3D11(_renderer_impl->render_device, _renderer_impl->device_context, _swap_chain_descriptor, Diligent::FullScreenModeDesc {}, _win32_native_window, &(_renderer_impl->swap_chain));
         } else if constexpr (is_renderer_backend_vulkan) {
         } else if constexpr (is_renderer_backend_opengl) {
             Diligent::IEngineFactoryOpenGL* _factory_ptr = Diligent::GetEngineFactoryOpenGL();
             Diligent::EngineGLCreateInfo _engine_create_info;
             _engine_create_info.Window = { existing_window.get_native_window() };
             // kidnap std::cout ? => go dans libconsole
-            _factory_ptr->CreateDeviceAndSwapChainGL(_engine_create_info, &_render_device_impl, &_device_context_impl, _swap_chain_descriptor, &_swap_chain_impl);
+            _factory_ptr->CreateDeviceAndSwapChainGL(_engine_create_info, &(_renderer_impl->render_device), &(_renderer_impl->device_context), _swap_chain_descriptor, &(_renderer_impl->swap_chain));
         }
 
-        const auto& SCDesc = _swap_chain_impl->GetDesc();
-        _imgui_renderer_impl = (new Diligent::ImGuiImplSDL(existing_window.get_sdl_window(), _render_device_impl, SCDesc.ColorBufferFormat, SCDesc.DepthBufferFormat));
+        const auto& SCDesc = _renderer_impl->swap_chain->GetDesc();
+        _renderer_impl->imgui_renderer = std::make_unique<Diligent::ImGuiImplSDL>(existing_window.get_sdl_window(), _renderer_impl->render_device, SCDesc.ColorBufferFormat, SCDesc.DepthBufferFormat);
     }
 
     renderer::~renderer()
@@ -104,19 +97,19 @@ namespace glue {
 
     void renderer::new_frame()
     {
-        auto* _rtv_ptr = _swap_chain_impl->GetCurrentBackBufferRTV();
-        auto* _dsv_ptr = _swap_chain_impl->GetDepthBufferDSV();
-        _device_context_impl->SetRenderTargets(1, &_rtv_ptr, _dsv_ptr, Diligent::RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
-        _device_context_impl->ClearRenderTarget(_rtv_ptr, _clear_color.data(), Diligent::RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
-        _device_context_impl->ClearDepthStencil(_dsv_ptr, Diligent::CLEAR_DEPTH_FLAG, 1.f, 0, Diligent::RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
-        const auto& _swap_chain_desc = _swap_chain_impl->GetDesc();
-        _imgui_renderer_impl->NewFrame(_swap_chain_desc.Width, _swap_chain_desc.Height, _swap_chain_desc.PreTransform);
+        auto* _rtv_ptr = _renderer_impl->swap_chain->GetCurrentBackBufferRTV();
+        auto* _dsv_ptr = _renderer_impl->swap_chain->GetDepthBufferDSV();
+        _renderer_impl->device_context->SetRenderTargets(1, &_rtv_ptr, _dsv_ptr, Diligent::RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
+        _renderer_impl->device_context->ClearRenderTarget(_rtv_ptr, _clear_color.data(), Diligent::RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
+        _renderer_impl->device_context->ClearDepthStencil(_dsv_ptr, Diligent::CLEAR_DEPTH_FLAG, 1.f, 0, Diligent::RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
+        const auto& _swap_chain_desc = _renderer_impl->swap_chain->GetDesc();
+        _renderer_impl->imgui_renderer->NewFrame(_swap_chain_desc.Width, _swap_chain_desc.Height, _swap_chain_desc.PreTransform);
     }
 
     void renderer::present()
     {
-        _imgui_renderer_impl->Render(_device_context_impl);
-        _swap_chain_impl->Present();
+        _renderer_impl->imgui_renderer->Render(_renderer_impl->device_context);
+        _renderer_impl->swap_chain->Present();
     }
 
     void renderer::process_input(const std::any& input)
@@ -126,11 +119,11 @@ namespace glue {
         if (_event->type == SDL_WINDOWEVENT) {
             if (_event->window.event == SDL_WINDOWEVENT_RESIZED) {
                 int _w, _h;
-                SDL_GetWindowSize(_sdl_window, &_w, &_h);
-                _swap_chain_impl->Resize(_w, _h);
+                SDL_GetWindowSize(_renderer_impl->sdl_window, &_w, &_h);
+                _renderer_impl->swap_chain->Resize(_w, _h);
             }
         }
-        _imgui_renderer_impl->ProcessEvent(&(std::any_cast<SDL_Event>(input)));
+        _renderer_impl->imgui_renderer->ProcessEvent(&(std::any_cast<SDL_Event>(input)));
     }
 
 }
