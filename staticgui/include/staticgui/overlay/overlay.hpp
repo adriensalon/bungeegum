8c3ef2727bb9d58bd1395cb5cd3cf08601a54cb6
adriensalon <adrien.salon@live.fr>
2023-02-08 12:29:50 +0100

    debug panel init

---
+++ staticgui/include/staticgui/overlay/overlay.hpp
@@ -0,0 +1,23 @@
+//       _        _   _                 _
+//      | |      | | (_)               (_)
+//   ___| |_ __ _| |_ _  ___ __ _ _   _ _
+//  / __| __/ _` | __| |/ __/ _` | | | | |
+//  \__ \ || (_| | |_| | (_| (_| | |_| | |
+//  |___/\__\__,_|\__|_|\___\__, |\__,_|_|
+//                           __/ |
+//                          |___/     v0.0
+
+#pragma once
+
+#include <staticgui/state/context.hpp>
+
+namespace staticgui {
+namespace detail {
+    namespace overlay {
+
+        void install_font();
+
+        void draw_overlay(context_state& context);
+    }
+}
+}
