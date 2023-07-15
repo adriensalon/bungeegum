cc3a2b5aa65f220f7ce4b1c660b88cc5948c586b
adriensalon <adrien.salon@live.fr>
2023-07-15 22:06:22 +0200

    dialog 2/3 pas mal

--- runtime/include/bungeegum/glue/dialog.hpp
+++ runtime/include/bungeegum/glue/dialog.hpp
@@ -8,6 +8,7 @@
 #include <bungeegum/glue/config.hpp>
 #include <bungeegum/glue/toolchain.hpp>
 
+/// Defines if we use desktop OS file picker dialogs.
 #if !defined(BUNGEEGUM_USE_DIALOG)
 #define BUNGEEGUM_USE_DIALOG (BUNGEEGUM_ENABLE_OVERLAY && (TOOLCHAIN_PLATFORM_WIN32 || TOOLCHAIN_PLATFORM_UWP || TOOLCHAIN_PLATFORM_MACOS || TOOLCHAIN_PLATFORM_LINUX))
 #endif
@@ -17,18 +18,28 @@
 namespace bungeegum {
 namespace detail {
 
-    /// @brief
+    /// @brief Instances of this struct represent filters to use with desktop OS file picker APIs.
     struct dialog_extensions_filter {
+
+        /// @brief Description for this filter, for example { "Libraries" }
         std::string description = {};
+
+        /// @brief List of extensions, for example { "lib", "dll" }
         std::vector<std::string> extensions = {};
     };
 
-    /// @brief
-    std::optional<std::filesystem::path> open_file_dialog(const std::vector<dialog_extensions_filter>& filters);
+    /// @brief Blocks the thread and gets a file path using desktop OS file picker APIs.
+    std::optional<std::filesystem::path> open_file_dialog(
+        const std::vector<dialog_extensions_filter>& filters = {},
+        const std::optional<std::filesystem::path>& default_path = std::nullopt);
 
-    /// @brief
-    std::optional<std::filesystem::path> save_file_dialog(const std::vector<dialog_extensions_filter>& filters);
+    /// @brief Blocks the thread and gets a file path for save using desktop OS file picker APIs.
+    std::optional<std::filesystem::path> save_file_dialog(
+        const std::vector<dialog_extensions_filter>& filters = {},
+        const std::optional<std::filesystem::path>& default_path = std::nullopt,
+        const std::optional<std::string>& default_name = std::nullopt);
 
+    // dir open ig
 }
 }
 
--- runtime/source/glue/dialog.cpp
+++ runtime/source/glue/dialog.cpp
@@ -1,5 +1,7 @@
 #include <bungeegum/glue/dialog.hpp>
 
+// F*CK C !
+
 #if BUNGEEGUM_USE_DIALOG
 
 #include <nfd.h>
@@ -7,11 +9,13 @@
 namespace bungeegum {
 namespace detail {
 
-    std::optional<std::filesystem::path> open_file_dialog(const std::vector<dialog_extensions_filter>& filters)
+    std::optional<std::filesystem::path> open_file_dialog(
+        const std::vector<dialog_extensions_filter>& filters,
+        const std::optional<std::filesystem::path>& default_path)
     {
+        // filters
         std::vector<std::string> _fused_extensions_filters = {};
         std::vector<nfdfilteritem_t> _nfd_filters = {};
-        nfdchar_t* _nfd_path = nullptr;
         for (const dialog_extensions_filter& _filter : filters) {
             std::string& _fused_extensions = _fused_extensions_filters.emplace_back();
             if (!_filter.extensions.empty()) {
@@ -22,20 +26,74 @@ namespace detail {
             }
             _nfd_filters.emplace_back(nfdfilteritem_t { _filter.description.c_str(), _fused_extensions.c_str() });
         }
-        nfdresult_t result = NFD_OpenDialog(&_nfd_path, _nfd_filters.data(), (nfdfiltersize_t)_nfd_filters.size(), NULL);
-        if (result == NFD_OKAY) {
-            std::string _retval(_nfd_path);
+
+        // default path
+        std::string _default_path_str;
+        const nfdchar_t* _nfd_default_path = NULL;
+        if (default_path.has_value()) {
+            _default_path_str = default_path.value().generic_string();
+            _nfd_default_path = _default_path_str.c_str();
+        }
+
+        // NFD call
+        nfdchar_t* _nfd_path = NULL;
+        nfdresult_t _nfd_result = NFD_OpenDialog(&_nfd_path, _nfd_filters.data(), (nfdfiltersize_t)_nfd_filters.size(), _nfd_default_path);
+
+        // result
+        if (_nfd_result == NFD_OKAY) {
+            std::string _result_str(_nfd_path);
             NFD_FreePath(_nfd_path);
-            return std::filesystem::path(_retval);
+            return std::filesystem::path(_result_str);
         } else {
             return std::nullopt;
         }
     }
 
-    std::optional<std::filesystem::path> save_file_dialog(const std::vector<dialog_extensions_filter>& filters)
+    std::optional<std::filesystem::path> save_file_dialog(
+        const std::vector<dialog_extensions_filter>& filters,
+        const std::optional<std::filesystem::path>& default_path,
+        const std::optional<std::string>& default_name)
     {
-        (void)filters;
-        return std::nullopt;
+        // filters
+        std::vector<std::string> _fused_extensions_filters = {};
+        std::vector<nfdfilteritem_t> _nfd_filters = {};
+        for (const dialog_extensions_filter& _filter : filters) {
+            std::string& _fused_extensions = _fused_extensions_filters.emplace_back();
+            if (!_filter.extensions.empty()) {
+                _fused_extensions += _filter.extensions.front();
+            }
+            for (std::size_t _k = 1u; _k < _filter.extensions.size(); ++_k) {
+                _fused_extensions += ", " + _filter.extensions[_k];
+            }
+            _nfd_filters.emplace_back(nfdfilteritem_t { _filter.description.c_str(), _fused_extensions.c_str() });
+        }
+
+        // default path
+        std::string _default_path_str;
+        const nfdchar_t* _nfd_default_path = NULL;
+        if (default_path.has_value()) {
+            _default_path_str = default_path.value().generic_string();
+            _nfd_default_path = _default_path_str.c_str();
+        }
+
+        // default name
+        const nfdchar_t* _nfd_default_name = NULL;
+        if (default_name.has_value()) {
+            _nfd_default_name = default_name.value().c_str();
+        }
+
+        // NFD call
+        nfdchar_t* _nfd_path = NULL;
+        nfdresult_t _nfd_result = NFD_SaveDialog(&_nfd_path, _nfd_filters.data(), (nfdfiltersize_t)_nfd_filters.size(), _nfd_default_path, _nfd_default_name);
+
+        // result
+        if (_nfd_result == NFD_OKAY) {
+            std::string _result_str(_nfd_path);
+            NFD_FreePath(_nfd_path);
+            return std::filesystem::path(_result_str);
+        } else {
+            return std::nullopt;
+        }
     }
 
 }
