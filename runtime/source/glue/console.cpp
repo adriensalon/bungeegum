8660847e6228aa8024ceed6f1b258270f5716f0e
adriensalon <adrien.salon@live.fr>
2023-08-14 01:24:48 +0200

    console xplatform

--- runtime/source/glue/console.cpp
+++ runtime/source/glue/console.cpp
@@ -20,49 +20,47 @@ namespace detail {
         {
             static_assert(std::is_same_v<string_t, std::string> || std::is_same_v<string_t, std::wstring>, "String type must be one of : std::string, std::wstring.");
 #if (TOOLCHAIN_PLATFORM_WIN32 || TOOLCHAIN_PLATFORM_UWP)
-            HANDLE _handle;
+            HANDLE _handle = GetStdHandle(STD_OUTPUT_HANDLE);
+            int _color = 7;
+            if (color == console_color::blue)
+                _color = 1;
+            else if (color == console_color::green)
+                _color = 2;
+            else if (color == console_color::cyan)
+                _color = 3;
+            else if (color == console_color::red)
+                _color = 4;
+            else if (color == console_color::magenta)
+                _color = 5;
+            else if (color == console_color::yellow)
+                _color = 6;
+            SetConsoleTextAttribute(_handle, static_cast<WORD>(_color));
+#elif (TOOLCHAIN_PLATFORM_LINUX || TOOLCHAIN_PLATFORM_MACOS)
+            std::string _color = "\033[0m";
+            if (color == console_color::blue)
+                _color = "\033[0;34m";
+            else if (color == console_color::green)
+                _color = "\033[0;32m";
+            else if (color == console_color::cyan)
+                _color = "\033[0;36m";
+            else if (color == console_color::red)
+                _color = "\033[0;31m";
+            else if (color == console_color::magenta)
+                _color = "\033[0;35m";
+            else if (color == console_color::yellow)
+                _color = "\033[0;33m";
+            std::cout << _color;
 #endif
-            if constexpr (is_platform_win32 || is_platform_uwp) {
-                _handle = GetStdHandle(STD_OUTPUT_HANDLE);
-                int _color = 7;
-                if (color == console_color::blue)
-                    _color = 1;
-                else if (color == console_color::green)
-                    _color = 2;
-                else if (color == console_color::cyan)
-                    _color = 3;
-                else if (color == console_color::red)
-                    _color = 4;
-                else if (color == console_color::magenta)
-                    _color = 5;
-                else if (color == console_color::yellow)
-                    _color = 6;
-                SetConsoleTextAttribute(_handle, static_cast<WORD>(_color));
-            } else if constexpr (is_platform_linux || is_platform_macos) {
-                std::string _color = "\033[0m";
-                if (color == console_color::blue)
-                    _color = "\033[0;34m";
-                else if (color == console_color::green)
-                    _color = "\033[0;32m";
-                else if (color == console_color::cyan)
-                    _color = "\033[0;36m";
-                else if (color == console_color::red)
-                    _color = "\033[0;31m";
-                else if (color == console_color::magenta)
-                    _color = "\033[0;35m";
-                else if (color == console_color::yellow)
-                    _color = "\033[0;33m";
-                std::cout << _color;
-            }
             if constexpr (std::is_same_v<string_t, std::string>) {
                 std::cout << message;
             } else if constexpr (std::is_same_v<string_t, std::wstring>) {
                 std::wcout << message;
             }
-            if constexpr (is_platform_win32 || is_platform_uwp)
-                SetConsoleTextAttribute(_handle, 7);
-            else if constexpr (is_platform_linux || is_platform_macos)
-                std::cout << "\033[0m";
+#if (TOOLCHAIN_PLATFORM_WIN32 || TOOLCHAIN_PLATFORM_UWP)
+            SetConsoleTextAttribute(_handle, 7);
+#elif (TOOLCHAIN_PLATFORM_LINUX || TOOLCHAIN_PLATFORM_MACOS)
+            std::cout << "\033[0m";
+#endif
         }
     }
 
